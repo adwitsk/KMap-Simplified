d7813dc88d62a1c1f35a6c8477378dbc6e77f3d6
adwitsk <72708694+adwitsk@users.noreply.github.com>
2020-10-12 21:24:42 +0530

    Create KMapper_testing.py

---
+++ KMapper_testing.py
@@ -0,0 +1,10 @@
+import unittest
+from HW2_2018276 import *
+
+class testpoint(unittest.TestCase):
+	def test_minFunc(self):
+     # TODO: Add tests.
+		
+                
+if __name__=='__main__':
+	unittest.main()
